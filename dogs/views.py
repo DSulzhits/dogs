19e7aef51421b4a738cab4bcca886a60b8726fd9
DSulzhits <dsulzhits@gmail.com>
2023-06-15 22:02:47 +0300

    Modified view DogUpdateView

--- dogs/views.py
+++ dogs/views.py
@@ -1,9 +1,10 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
-from dogs.forms import DogForm
-from dogs.models import Category, Dog
+from dogs.forms import DogForm, ParentForm
+from dogs.models import Category, Dog, Parent
 
 
 def index(request):
@@ -41,4 +42,26 @@ class DogCreateView(CreateView):
 class DogUpdateView(UpdateView):
     model = Dog
     form_class = DogForm
-    success_url = reverse_lazy('dogs:categories')
+
+    def get_success_url(self):
+        return reverse('dogs:dog_update', args=[self.kwargs.get('pk')])
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        ParentFormset = inlineformset_factory(Dog, Parent, form=ParentForm, extra=1)
+        if self.request.method == 'POST':
+            formset = ParentFormset(self.request.POST, instance=self.object)
+        else:
+            formset = ParentFormset(instance=self.object)
+        context_data['formset'] = formset
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        self.object = form.save()
+
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+        return super().form_valid(form)
