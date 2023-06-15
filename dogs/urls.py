2149e675627e2a55f28474511754619f216a5890
DSulzhits <dsulzhits@gmail.com>
2023-06-15 20:34:35 +0300

    add required urls

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from dogs.views import index, categories, category_dogs
+from dogs.views import index, categories, category_dogs, DogCreateView, DogUpdateView
 from dogs.apps import DogsConfig
 
 app_name = DogsConfig.name
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('categories/', categories, name='categories'),
     path('<int:pk>/dogs/', category_dogs, name='category_dogs'),
+    path('<int:pk>/dogs/create/', DogCreateView.as_view(), name='dog_create'),
+    path('/dogs/<int:pk>/update/', DogUpdateView.as_view(), name='dog_update')
 ]
