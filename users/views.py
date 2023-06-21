355233e168d5d3863cf85330d6dc91ee3fd3bbb5
DSulzhits <dsulzhits@gmail.com>
2023-06-21 17:38:18 +0300

    add password-generate function

--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
 from django.core.mail import send_mail
-from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
 from config import settings
 from users.forms import UserRegisterForm, UserForm
 from users.models import User
+import random
 
 
 class LoginView(BaseLoginView):
@@ -43,3 +45,16 @@ class UserUpdateView(UpdateView):
 
     def get_object(self, queryset=None):  # Позволяет не передавать доп. информацию, а редактирует того кто зашел
         return self.request.user
+
+
+def generate_new_password(request):
+    new_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+    send_mail(
+        subject='Вы сменили пароль',
+        message=f'Ваш новый пароль: {new_password}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email]
+    )
+    request.user.set_password(new_password)
+    request.user.save()
+    return redirect(reverse('dogs:index'))
