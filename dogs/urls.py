52115f5483ef986391ea86101528ca76f45d39a6
DSulzhits <dsulzhits@gmail.com>
2023-06-30 15:36:52 +0300

    add cash-setting never_cash

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page, never_cache
 
 from dogs.views import index, categories, DogListView, DogCreateView, DogUpdateView
 from dogs.apps import DogsConfig
@@ -11,5 +11,5 @@ urlpatterns = [
     path('categories/', categories, name='categories'),
     path('<int:pk>/dogs/', DogListView.as_view(), name='category_dogs'),
     path('<int:pk>/dogs/create/', DogCreateView.as_view(), name='dog_create'),
-    path('dogs/<int:pk>/update/', DogUpdateView.as_view(), name='dog_update')
+    path('dogs/<int:pk>/update/', never_cache(DogUpdateView.as_view()), name='dog_update')
 ]
