2ebeb322927ce7bb9def368c14eb43056bc4773a
DSulzhits <dsulzhits@gmail.com>
2023-06-30 15:32:03 +0300

    add cash settings for page index

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from dogs.views import index, categories, DogListView, DogCreateView, DogUpdateView
 from dogs.apps import DogsConfig
@@ -6,7 +7,7 @@ from dogs.apps import DogsConfig
 app_name = DogsConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', cache_page(60)(index), name='index'),
     path('categories/', categories, name='categories'),
     path('<int:pk>/dogs/', DogListView.as_view(), name='category_dogs'),
     path('<int:pk>/dogs/create/', DogCreateView.as_view(), name='dog_create'),
