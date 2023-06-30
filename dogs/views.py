f3dca82362befbc8b86c63d32c93f5c483579ff0
DSulzhits <dsulzhits@gmail.com>
2023-06-30 15:32:32 +0300

    add cash settings in controller def categories(request):

--- dogs/views.py
+++ dogs/views.py
@@ -5,6 +5,8 @@ from django.http import Http404
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView, ListView
+from django.core.cache import cache
+from django.conf import settings
 
 from dogs.forms import DogForm, ParentForm
 from dogs.models import Category, Dog, Parent
@@ -21,8 +23,16 @@ def index(request):
 
 @login_required
 def categories(request):
+    if settings.CACHE_ENABLED:
+        key = 'category_list'
+        category_list = cache.get(key)
+        if category_list is None:
+            category_list = Category.objects.all()
+            cache.set(key, category_list)
+    else:
+        category_list = Category.objects.all()
     context = {
-        'object_list': Category.objects.all(),
+        'object_list': category_list,
         'title': 'Питомник - Все наши породы',
     }
     return render(request, 'dogs/categories.html', context)
